e803f81b3f350d20a6fad314c6faedf1d65eba5d
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-04-14 11:52:33 -0600

    feat: se agrego logica al service que va a tener comunicacion con el implements

--- shop/src/main/java/com/joelarchila/shop/service/UsuarioService.java
+++ shop/src/main/java/com/joelarchila/shop/service/UsuarioService.java
@@ -5,6 +5,14 @@ import com.joelarchila.shop.entity.Usuario;
 import java.util.List;
 
 public interface UsuarioService {
+    Usuario registrar(String usuario, String password);
+
+    Usuario login(String usuario, String password);
+
+    List<Usuario> listar();
+
+    void eliminar(int id);
+
     List<Usuario> getAllUsuarios();
     Usuario getUsuarioById(Integer id);
     Usuario saveUsuario(Usuario usuario) throws RuntimeException;
