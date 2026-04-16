ca2f897f26f8df172c0d0ac6065febb5f6614731
joelArchila-2022283 <jarchila-2022283@kinal.edu.gt>
2026-04-15 23:50:39 -0600

    test: se cambiaron cosas en la estructura

--- shop/src/main/java/com/joelarchila/shop/service/DetalleVentaService.java
+++ shop/src/main/java/com/joelarchila/shop/service/DetalleVentaService.java
@@ -5,8 +5,8 @@ import com.joelarchila.shop.entity.DetalleVenta;
 import java.util.List;
 
 public interface DetalleVentaService {
-    List<DetalleVenta> getAllDetalles();
-    DetalleVenta getDetalleById(Integer id);
-    DetalleVenta saveDetalle(DetalleVenta detalle) throws RuntimeException;
-    void deleteDetalle(Integer id);
+    List<DetalleVenta> getAllDetalleVentas();
+    DetalleVenta getDetalleVentaById(Integer id);
+    DetalleVenta saveDetalleVenta(DetalleVenta detalleVenta);
+    void deleteDetalleVenta(Integer id);
 }
